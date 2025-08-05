8ddf30d3320e33adf167e2ce27b765fa1adc476c
Gabriel Poca <mail@gabrielpoca.com>
2025-08-05 17:09:23 +0100

    clean-up from last commit

--- db/local.ts
+++
@@ -1,9 +0,0 @@
-import Database from 'better-sqlite3';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-
-import * as schema from './schema-local';
-
-// Create SQLite database for local development
-const sqlite = new Database('./local.db');
-
-export const db = drizzle(sqlite, { schema });
