658e650bcb14bdbb55f3bdf69728cf6240556195
josevazf <josevaz.fco@gmail.com>
2025-08-07 12:15:36 +0100

    Remove test

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,16 +1,10 @@
 import { useWalletGate } from '@/hooks/useWalletGate';
 import WalletGate from '@/components/layout/WalletGate';
-import { saveUser } from '../api/user';
 
 export function Home() {
   const { isConnected } = useWalletGate();
 
   if (!isConnected) {
-    saveUser({
-      id: `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`,
-      mainEvm: '0x1234567890123456789012345678901234567890',
-      referrerCode: '1234567890',
-    });
     return <WalletGate />;
   }
   return null;
