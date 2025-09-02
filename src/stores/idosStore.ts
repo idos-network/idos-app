587d45aacbe1189e37c2f031ee8775dd9ffbd5c1
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-09-02 13:14:52 +0300

    (idos-app): clear all queries on logout

--- src/stores/idosStore.ts
+++ src/stores/idosStore.ts
@@ -1,7 +1,8 @@
-import { createIDOSClient, type idOSClient } from '@idos-network/client';
+import { queryClient } from '@/providers/tanstack-query/query-client';
+import { idOSClientConfiguration, type idOSClient } from '@idos-network/client';
 import { create } from 'zustand';
 
-export const _idOSClient = createIDOSClient({
+export const _idOSClient = new idOSClientConfiguration({
   nodeUrl: 'https://nodes.staging.idos.network/',
   enclaveOptions: {
     container: '#idOS-enclave',
@@ -26,6 +27,7 @@ export const useIdosStore = create<IdosStore>((set) => ({
       const idleClient = await idOSClient.logOut();
       set({ idOSClient: idleClient, initializing: true });
       window.location.reload();
+      queryClient.removeQueries()
     }
   },
 }));
