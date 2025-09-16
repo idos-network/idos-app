ef752f199ed9d9692552690cfb4961cce1ad679e
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-09-16 11:02:45 +0300

    fix(idos-app): update cookie content

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -18,14 +18,11 @@ const CookieBanner = () => {
     <div className="cookies_banner flex fixed bottom-0 left-0 right-0 bg-idos-grey1 color-ui-text-light  z-1000 ">
       <div className="cookies_banner-container w-full flex flex-row justify-center align-start gap-5 p-6">
         <div className="cookies_banner-disclaimer text-sm color-ui-text-light max-w-7xl">
-          We do not use <span className="font-bold">non-essential cookies</span>{' '}
-          on this website, but we do collect some important data in order to
-          properly customize the website for you. We collect the URL of the
-          website you visited before our website, bounce rate, session record,
-          time spent on the site and sub-pages, mouse events, your device type
-          and browser information. If you do not consent to the collection of
-          the above data, click "decline".
-          <br />
+          We do not use{' '}
+          <span className="font-bold">non-essential cookies.</span> By pressing
+          the approving button I voluntarily give my consent to set or activate
+          external connections. I know their functions because they are
+          described in the{' '}
           <a
             href="https://www.idos.network/legal/privacy-policy"
             className="footer_block-link"
@@ -36,6 +33,24 @@ const CookieBanner = () => {
           >
             Privacy Policy
           </a>
+          {', '}or explained in more detail in documents or external links
+          implemented there. I have the right to withdraw my data protection
+          consent at any time with effect for the future, by changing my cookie
+          preferences or deleting my cookies. The withdrawal of consent shall
+          not affect the lawfulness of processing based on consent before its
+          withdrawal. With my action I also confirm that I have read and taken
+          note of the Privacy Policy and the{' '}
+          <a
+            href="https://drive.google.com/file/d/1lzrdgD_dwusE4xsKw_oTUcu8Hq3YU60b/view"
+            className="footer_block-link"
+            style={{
+              color: '#7a7a7a',
+              textDecoration: 'none',
+            }}
+          >
+            Transparency Document
+          </a>
+          .
         </div>
 
         <div className="cookies_banner-buttons flex gap-3 items-center">
