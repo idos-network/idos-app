d59b49af410fba6e43d591b6c663742eb4c3e6a9
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-09-16 12:42:02 +0300

    fix(idos-app): update migration

--- migrations/0000_sticky_kylun.sql
+++ migrations/0000_sticky_kylun.sql
@@ -6,10 +6,25 @@ CREATE TABLE "user_quests" (
 	"updatedAt" timestamp DEFAULT now()
 );
 --> statement-breakpoint
+CREATE TABLE "user_wallets" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "user_wallets_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"userId" varchar(36) NOT NULL,
+	"address" varchar NOT NULL,
+	"walletType" varchar NOT NULL,
+	"createdAt" timestamp DEFAULT now(),
+	"updatedAt" timestamp DEFAULT now(),
+	CONSTRAINT "user_wallets_user_address_unique" UNIQUE("userId","address")
+);
+--> statement-breakpoint
 CREATE TABLE "users" (
 	"id" varchar(36) PRIMARY KEY NOT NULL,
 	"mainEvm" varchar(255) DEFAULT '',
 	"referrerCode" varchar DEFAULT '',
+	"cookieConsent" boolean DEFAULT null,
+	"faceSignToken" varchar,
+	"faceSignTokenCreatedAt" timestamp,
+	"faceSignDone" boolean DEFAULT false,
+	"faceSignHash" varchar(300),
 	"createdAt" timestamp DEFAULT now(),
 	"updatedAt" timestamp DEFAULT now(),
 	CONSTRAINT "users_id_unique" UNIQUE("id")
@@ -18,5 +33,8 @@ CREATE TABLE "users" (
 CREATE INDEX "user_quests_user_id_idx" ON "user_quests" USING btree ("userId");--> statement-breakpoint
 CREATE INDEX "user_quests_quest_name_idx" ON "user_quests" USING btree ("questName");--> statement-breakpoint
 CREATE INDEX "user_quests_user_quest_idx" ON "user_quests" USING btree ("userId","questName");--> statement-breakpoint
+CREATE INDEX "user_wallets_user_id_idx" ON "user_wallets" USING btree ("userId");--> statement-breakpoint
+CREATE INDEX "user_wallets_address_idx" ON "user_wallets" USING btree ("address");--> statement-breakpoint
+CREATE INDEX "user_wallets_wallet_type_idx" ON "user_wallets" USING btree ("walletType");--> statement-breakpoint
 CREATE INDEX "main_evm_idx" ON "users" USING btree ("mainEvm");--> statement-breakpoint
 CREATE INDEX "referrer_code_idx" ON "users" USING btree ("referrerCode");
--- migrations/0002_green_frightful_four.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "faceSignToken" varchar;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "faceSignTokenCreatedAt" timestamp;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "faceSignDone" boolean DEFAULT false;
