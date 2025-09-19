cb79b3dea74d5fb653163c911660beac495ca635
josevazf <josevaz.fco@gmail.com>
2025-09-19 21:37:39 +0100

    Update referral card

--- src/routes/Points.tsx
+++ src/routes/Points.tsx
@@ -1,6 +1,6 @@
 import PointsFrame from '@/components/points/PointsFrame';
 import QuestsCard from '@/components/points/QuestsCard';
-import ReferralLink from '@/components/points/ReferralLink';
+import ReferralCard from '@/components/points/ReferralCard';
 import Spinner from '@/components/Spinner';
 import { useProfileQuestCompleted } from '@/hooks/useProfileQuestCompleted';
 import { useNavigate } from '@tanstack/react-router';
@@ -45,7 +45,7 @@ export function Points() {
 
   return (
     <div className="flex items-start justify-center">
-      <div className="container mx-auto max-w-[1050px] flex flex-col px-32 pt-10">
+      <div className="container mx-auto max-w-[1250px] flex flex-col px-32 pt-10">
         <div className="gap-3 flex flex-col mb-10">
           <div className="text-2xl font-medium text-neutral-50">
             idOS Points*
@@ -56,14 +56,16 @@ export function Points() {
         </div>
         <div className="flex gap-5 justify-between">
           <PointsFrame />
-          <ReferralLink />
         </div>
 
         <div className="flex flex-col gap-6 justify-between">
           <div className="flex gap-5 font-medium text-xl text-neutral-50 justify-between mt-10">
-            Complete quests to earn points
+            Complete quests and refer your friens to get Quest Points
           </div>
-          <QuestsCard />
+          <div className="flex gap-5">
+            <QuestsCard />
+            <ReferralCard />
+          </div>
         </div>
         <PointsDisclaimer />
       </div>
