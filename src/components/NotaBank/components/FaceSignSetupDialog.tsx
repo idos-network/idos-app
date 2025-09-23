738ebb571df590f2206725aa7471627e01ac67ad
Jan Strnádek <jan.strnadek@gmail.com>
2025-09-23 10:08:22 +0200

    fix(lint): FaceSign lint

--- src/components/NotaBank/components/FaceSignSetupDialog.tsx
+++ src/components/NotaBank/components/FaceSignSetupDialog.tsx
@@ -103,7 +103,7 @@ export default function FaceSignSetupDialog({
             }
             setQrCodeView(false);
             setFaceSignResult(true);
-            onDone(true)
+            onDone(true);
           }
         })
         .catch((error) => {
