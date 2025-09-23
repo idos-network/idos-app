e2a60deaa6d3bf44ed97b47882251ad8179d9af1
Jan Strnádek <jan.strnadek@gmail.com>
2025-09-23 18:03:35 +0200

    fix(db): Merge requests

--- migrations/0003_messy_blockbuster.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "popCredentialsId" varchar DEFAULT null;--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "faceSignDone";
---
+++ migrations/0005_opposite_spiral.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "users" ADD COLUMN "popCredentialId" varchar DEFAULT null;--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "faceSignDone";
