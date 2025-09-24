ee0b4938439546959b69b7e03d5a86cf1a389586
Jan Strnádek <jan.strnadek@gmail.com>
2025-09-24 16:09:23 +0200

    fix(revert): Revert local change

--- src/utils/facetec/index.ts
+++ src/utils/facetec/index.ts
@@ -39,8 +39,8 @@ export class FaceTecContainer {
 
     this.FaceTecSDK.setResourceDirectory('/facetec/FaceTecSDK.js/resources');
     this.FaceTecSDK.setImagesDirectory('/facetec/FaceTecSDK.js/FaceTec_images');
-    this.FaceTecSDK.initializeInDevelopmentMode(
-      // env.VITE_FACETEC_PRODUCTION_KEY,
+    this.FaceTecSDK.initializeInProductionMode(
+      env.VITE_FACETEC_PRODUCTION_KEY,
       env.VITE_FACETEC_DEVICE_KEY_IDENTIFIER,
       publicKey,
       (initializationSuccess: boolean) => {
