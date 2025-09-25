e0daba0b9d50a4bf8aea3940cb5961003ca6d616
Jan Strnádek <jan.strnadek@gmail.com>
2025-09-25 09:19:22 +0200

    feat(facetec): Better facetec loading

--- functions/face-sign-onboard.ts
+++ functions/face-sign-onboard.ts
@@ -6,70 +6,75 @@ import { createResponse } from './utils/response';
 const facetecServer = process.env.FACETEC_SERVER as string;
 
 export default async (request: Request, context: Context) => {
-  // TODO: Get userID from token or session or whatever is available
-  const { userId } = context.params;
-
-  if (!userId) {
-    throw new UserNotFoundError(userId);
-  }
-
-  const { faceScan, key, userAgent, auditTrailImage, lowQualityAuditTrailImage, sessionId } = await request.json();
-
-  const response = await fetch(`${facetecServer}login`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      key,
-      userAgent,
-      faceScan,
-      auditTrailImage,
-      lowQualityAuditTrailImage,
-      sessionId,
-    }),
-  });
-
-  const json = await response.json();
-
-  if (!response.ok) {
-    return createResponse(
-      {
-        error: true,
-        wasProcessed: false,
-        errorMessage: json.errorMessage ?? `FaceSign server error: ${response.statusText}`,
-      },
-      500
-    );
-  }
-
-  if (!json.success) {
-    return createResponse({
-      wasProcessed: false,
-      success: false,
-      error: json.error,
-      errorMessage: json.errorMessage,
-    }, 400);
-  }
-
   try {
-    await updateUserFaceSign(userId, json.faceSignUserId);
+    // TODO: Get userID from token or session or whatever is available
+    const { userId } = context.params;
 
-    return createResponse({
-      success: true,
-      wasProcessed: true,
-      error: false,
-      duplicate: false,
-      scanResultBlob: json.scanResultBlob,
-    }, 200);
+    if (!userId) {
+      throw new UserNotFoundError(userId);
+    }
+
+    const { faceScan, key, userAgent, auditTrailImage, lowQualityAuditTrailImage, sessionId } = await request.json();
+
+    const response = await fetch(`${facetecServer}login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        key,
+        userAgent,
+        faceScan,
+        auditTrailImage,
+        lowQualityAuditTrailImage,
+        sessionId,
+      }),
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      return createResponse(
+        {
+          error: true,
+          wasProcessed: false,
+          errorMessage: json.errorMessage ?? `FaceSign server error: ${response.statusText}`,
+        },
+        500
+      );
+    }
+
+    if (!json.success) {
+      return createResponse({
+        wasProcessed: false,
+        success: false,
+        error: json.error,
+        errorMessage: json.errorMessage,
+      }, 400);
+    }
+
+    try {
+      await updateUserFaceSign(userId, json.faceSignUserId);
+
+      return createResponse({
+        success: true,
+        wasProcessed: true,
+        error: false,
+        duplicate: false,
+        scanResultBlob: json.scanResultBlob,
+      }, 200);
+    } catch (e) {
+      return createResponse({
+        success: false,
+        wasProcessed: true,
+        error: true,
+        duplicate: true,
+        errorMessage: "Sorry, you have already created an idOS profile with FaceSign on another wallet. Please disconnect and reconnect with that original wallet.",
+      }, 400);
+
+    }
   } catch (e) {
-    return createResponse({
-      success: false,
-      wasProcessed: true,
-      error: true,
-      duplicate: true,
-      errorMessage: "Sorry, you have already created an idOS profile with FaceSign on another wallet. Please disconnect and reconnect with that original wallet.",
-    }, 400);
+    console.error('FaceSign onboard error', e);
   }
 };
 
