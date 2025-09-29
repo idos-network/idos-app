f504190d4490a75d581b70ff7d87960a861b711c
José Vaz Francisco <josevaz.fco@gmail.com>
2025-09-29 11:17:33 +0100

    Fix: add credential (#220)

--- functions/idos-credential.ts
+++ functions/idos-credential.ts
@@ -4,136 +4,152 @@ import { idOSIssuer as idOSIssuerClass } from '@idos-network/issuer';
 import type { Config, Context } from '@netlify/functions';
 import { encode as utf8Encode } from '@stablelib/utf8';
 import nacl from 'tweetnacl';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import { Pool } from '@neondatabase/serverless';
 
-export default async (request: Request, _context: Context) => {
-  if (request.method !== 'POST') {
-    return new Response(
-      JSON.stringify({
-        success: false,
-        message: 'Method not allowed. Use POST.',
-      }),
-    );
-  }
+export default async (request: Request, context: Context) => {
+  const pool = new Pool({ connectionString: process.env.NETLIFY_DATABASE_URL });
+  const db = drizzle(pool);
+  try {
+    if (request.method !== 'POST') {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: 'Method not allowed. Use POST.',
+        }),
+      );
+    }
 
-  const { idOSDWG, userEncryptionPublicKey, userId } =
-    (await request.json()) as {
-      idOSDWG: IdosDWG;
-      userEncryptionPublicKey: string;
-      userId: string;
+    const { idOSDWG, userEncryptionPublicKey, userId } =
+      (await request.json()) as {
+        idOSDWG: IdosDWG;
+        userEncryptionPublicKey: string;
+        userId: string;
+      };
+
+    const user = await getUserById(userId).then((res) => res[0]);
+
+    if (!user) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: 'User not found.',
+        }),
+        { status: 404 },
+      );
+    }
+
+    if (!user.faceSignUserId || user.popCredentialsId) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message:
+            'User is in invalid state (did not complete face sign, or already has POP credentials).',
+        }),
+        { status: 400 },
+      );
+    }
+
+    const idOSIssuer = await idOSIssuerClass.init({
+      nodeUrl: process.env.VITE_IDOS_NODE_URL as string,
+      signingKeyPair: nacl.sign.keyPair.fromSecretKey(
+        Buffer.from(process.env.ISSUER_SIGNING_SECRET_KEY as string, 'hex'),
+      ),
+      encryptionSecretKey: Buffer.from(
+        process.env.ISSUER_ENCRYPTION_SECRET_KEY as string,
+        'hex',
+      ),
+    });
+
+    let issuerDomain = process.env.FACETEC_SERVER as string;
+    if (issuerDomain.endsWith('/')) {
+      issuerDomain = issuerDomain.slice(0, -1);
+    }
+
+    const credentialFields = {
+      id: `${issuerDomain}/credentials/${user.faceSignUserId}`,
+      level: 'human',
+      issued: new Date(),
+      approvedAt: new Date(),
     };
 
-  const user = await getUserById(userId).then((res) => res[0]);
+    const issuer = {
+      issuer: `${issuerDomain}/idos`,
+      publicKeyMultibase: process.env.ISSUER_PUBLIC_KEY_MULTIBASE as string,
+      privateKeyMultibase: process.env.ISSUER_PRIVATE_KEY_MULTIBASE as string,
+    };
 
-  if (!user) {
+    const plainContent = await idOSIssuer.buildFaceIdCredential(
+      credentialFields,
+      {
+        faceSignUserId: user.faceSignUserId,
+      },
+      issuer,
+    );
+
+    const publicNotes = {
+      // `id` is required to make `editCredential` work.
+      id: crypto.randomUUID(),
+      type: 'PoP',
+      level: 'human',
+      status: 'approved',
+      issuer: 'FaceSign',
+    };
+
+    if (!userEncryptionPublicKey) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: 'User encryption public key is required.',
+        }),
+        { status: 400 },
+      );
+    }
+
+    const recipientEncryptionPublicKey = Buffer.from(
+      userEncryptionPublicKey,
+      'base64',
+    );
+
+    const credentialParams = {
+      publicNotes: JSON.stringify(publicNotes),
+      plaintextContent: utf8Encode(JSON.stringify(plainContent)),
+      recipientEncryptionPublicKey: recipientEncryptionPublicKey,
+    };
+
+    const dwgParams = {
+      id: idOSDWG.delegatedWriteGrant.id,
+      ownerWalletIdentifier:
+        idOSDWG.delegatedWriteGrant.owner_wallet_identifier,
+      consumerWalletIdentifier:
+        idOSDWG.delegatedWriteGrant.grantee_wallet_identifier,
+      issuerPublicKey: idOSDWG.delegatedWriteGrant.issuer_public_key,
+      accessGrantTimelock: idOSDWG.delegatedWriteGrant.access_grant_timelock,
+      notUsableBefore: idOSDWG.delegatedWriteGrant.not_usable_before,
+      notUsableAfter: idOSDWG.delegatedWriteGrant.not_usable_after,
+      signature: idOSDWG.signature,
+    };
+
+    await db.transaction(async (tx: any) => {
+      await tx.execute('LOCK TABLE lock_table IN EXCLUSIVE MODE');
+      const result = await idOSIssuer.createCredentialByDelegatedWriteGrant(
+        credentialParams,
+        dwgParams,
+      );
+      await setUserPopCredentialId(userId, result.originalCredential.id);
+    });
+
     return new Response(
       JSON.stringify({
-        success: false,
-        message: 'User not found.',
+        success: true,
+        message: 'Credential created successfully',
       }),
-      { status: 404 },
+      { status: 200 },
     );
+  } finally {
+    // Ensure pool is closed after request
+    context.waitUntil(pool.end());
   }
-
-  if (!user.faceSignUserId || user.popCredentialsId) {
-    return new Response(
-      JSON.stringify({
-        success: false,
-        message:
-          'User is in invalid state (did not complete face sign, or already has POP credentials).',
-      }),
-      { status: 400 },
-    );
-  }
-
-  const idOSIssuer = await idOSIssuerClass.init({
-    nodeUrl: process.env.VITE_IDOS_NODE_URL as string,
-    signingKeyPair: nacl.sign.keyPair.fromSecretKey(
-      Buffer.from(process.env.ISSUER_SIGNING_SECRET_KEY as string, 'hex'),
-    ),
-    encryptionSecretKey: Buffer.from(
-      process.env.ISSUER_ENCRYPTION_SECRET_KEY as string,
-      'hex',
-    ),
-  });
-
-  let issuerDomain = process.env.FACETEC_SERVER as string;
-  if (issuerDomain.endsWith('/')) {
-    issuerDomain = issuerDomain.slice(0, -1);
-  }
-
-  const credentialFields = {
-    id: `${issuerDomain}/credentials/${user.faceSignUserId}`,
-    level: 'human',
-    issued: new Date(),
-    approvedAt: new Date(),
-  };
-
-  const issuer = {
-    issuer: `${issuerDomain}/idos`,
-    publicKeyMultibase: process.env.ISSUER_PUBLIC_KEY_MULTIBASE as string,
-    privateKeyMultibase: process.env.ISSUER_PRIVATE_KEY_MULTIBASE as string,
-  };
-
-  const plainContent = await idOSIssuer.buildFaceIdCredential(
-    credentialFields,
-    {
-      faceSignUserId: user.faceSignUserId,
-    },
-    issuer,
-  );
-
-  const publicNotes = {
-    // `id` is required to make `editCredential` work.
-    id: crypto.randomUUID(),
-    type: 'PoP',
-    level: 'human',
-    status: 'approved',
-    issuer: 'FaceSign',
-  };
-
-  if (!userEncryptionPublicKey) {
-    return new Response(
-      JSON.stringify({
-        success: false,
-        message: 'User encryption public key is required.',
-      }),
-      { status: 400 },
-    );
-  }
-
-  const recipientEncryptionPublicKey = Buffer.from(
-    userEncryptionPublicKey,
-    'base64',
-  );
-
-  const credentialParams = {
-    publicNotes: JSON.stringify(publicNotes),
-    plaintextContent: utf8Encode(JSON.stringify(plainContent)),
-    recipientEncryptionPublicKey: recipientEncryptionPublicKey,
-  };
-
-  const dwgParams = {
-    id: idOSDWG.delegatedWriteGrant.id,
-    ownerWalletIdentifier: idOSDWG.delegatedWriteGrant.owner_wallet_identifier,
-    consumerWalletIdentifier:
-      idOSDWG.delegatedWriteGrant.grantee_wallet_identifier,
-    issuerPublicKey: idOSDWG.delegatedWriteGrant.issuer_public_key,
-    accessGrantTimelock: idOSDWG.delegatedWriteGrant.access_grant_timelock,
-    notUsableBefore: idOSDWG.delegatedWriteGrant.not_usable_before,
-    notUsableAfter: idOSDWG.delegatedWriteGrant.not_usable_after,
-    signature: idOSDWG.signature,
-  };
-
-  const result = await idOSIssuer.createCredentialByDelegatedWriteGrant(
-    credentialParams,
-    dwgParams,
-  );
-
-  await setUserPopCredentialId(userId, result.originalCredential.id);
-
-  // await setUserName(userId);
-
-  return new Response(JSON.stringify(result), { status: 200 });
 };
 
 export const config: Config = {
