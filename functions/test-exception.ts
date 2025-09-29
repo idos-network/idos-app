7ea35dcad43e57a760843800265225280d218b03
Jan Strnádek <jan.strnadek@gmail.com>
2025-09-29 18:43:27 +0200

    feat(sentry) Serverless functions

--- functions/test-exception.ts
+++ functions/test-exception.ts
@@ -1,10 +1,18 @@
 import { UserNotFoundError } from '@/utils/errors';
-import { withSentry } from "@neptune.digital/sentry-netlify-v2";
-import type { Config, Context } from '@netlify/functions';
+import type { Config } from '@netlify/functions';
+import * as Sentry from "@sentry/aws-serverless";
 
-export default withSentry(async (_request: Request, _context: Context) => {
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0, // nebo míň v produkci
+});
+
+
+export default Sentry.wrapHandler<Request, never>(async (_request: Request) => {
   throw new UserNotFoundError("TEST_USER_ID");
 });
+
+// export default Sentry.wrapHandler(handler)
 
 export const config: Config = {
   path: '/api/test-exception',
