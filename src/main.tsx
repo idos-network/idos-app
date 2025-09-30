6fa8da9e90a3b5151c1dc2500a0afe563a949711
Jan Strnádek <jan.strnadek@gmail.com>
2025-09-30 16:48:30 +0200

    feat(cookie) Change hook to provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,8 @@ import { createRoot } from 'react-dom/client';
 import { createRouter } from './router';
 import './styles/app.css';
 import './styles/facetec-custom.css';
-import { env } from './env';
-import ReactGA from 'react-ga';
 
 const router = createRouter();
-
-if (env.VITE_GA_TRACKING_ID) {
-  ReactGA.initialize(env.VITE_GA_TRACKING_ID);
-  router.subscribe("onLoad", ({ toLocation }) => {
-    ReactGA.pageview(toLocation.pathname);
-  });
-}
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
