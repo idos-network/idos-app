1c596b33d5198d7700398c489f59b9ef3b67b236
ZePedroResende <resende@subvisual.co>
2025-10-01 23:56:40 +0100

    fmt

--- src/api/subscribe-newsletter.ts
+++ src/api/subscribe-newsletter.ts
@@ -1,10 +1,8 @@
 import axiosInstance from './axios';
 
-export const subscribeNewsletter = async (
-  email: string,
-) => {
+export const subscribeNewsletter = async (email: string) => {
   const response = await axiosInstance.post('/subscribe-newsletter', {
-    email
+    email,
   });
 
   return response.data;
