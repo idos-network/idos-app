f91194a132553deb2635682992d837d639e2fd7a
Jan Strnádek <jan.strnadek@gmail.com>
2025-10-01 14:55:31 +0200

    feat(idos) Add more logs

--- functions/utils/idos-issuer.ts
+++ functions/utils/idos-issuer.ts
@@ -1,5 +1,6 @@
 import { idOSIssuer as idOSIssuerClass } from '@idos-network/issuer';
 import nacl from 'tweetnacl';
+import { type KwilSigner, NodeKwil, WebKwil } from "@idos-network/kwil-js";
 
 export async function issuerWithKey() {
   const keys = process.env.ISSUER_SIGNING_SECRET_KEYS?.split(",").map(k => k.trim());
@@ -14,17 +15,28 @@ export async function issuerWithKey() {
   const keyIndex = Math.floor(Math.random() * keyCount) + 1;
   console.log(`[idOS] Using key index: ${keyIndex} - ${keys[keyIndex - 1]?.slice(0, 4)}...`);
 
+  const signingKeyPair = nacl.sign.keyPair.fromSecretKey(
+    Buffer.from(keys[keyIndex - 1], 'hex'),
+  );
+
   // Randomize the account to avoid the nonce issue
   const idOSIssuer = await idOSIssuerClass.init({
     nodeUrl: process.env.VITE_IDOS_NODE_URL as string,
-    signingKeyPair: nacl.sign.keyPair.fromSecretKey(
-      Buffer.from(keys[keyIndex - 1], 'hex'),
-    ),
+    signingKeyPair,
     encryptionSecretKey: Buffer.from(
       process.env.ISSUER_ENCRYPTION_SECRET_KEY as string,
       'hex',
     ),
   });
+
+  // TODO: Remove after the issue is solved
+  const client = new NodeKwil({
+    kwilProvider: process.env.VITE_IDOS_NODE_URL as string,
+    chainId: "",
+  });
+  const account = await client.getAccount(signingKeyPair.publicKey);
+  console.log("[idOS] Using account:", JSON.stringify(account));
+  // TODO: Remove after the issue is solved
 
   return {
     keyLock: `idos_issuer_key_${keyIndex}`,
